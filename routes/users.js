d2bbd2ca2a032d1d71912a3b263860387f94861d
JChase2 <mr.jdchase@gmail.com>
2020-05-29 01:06:34 -0400

    Last assignment for week 3 done, added admin flag checks and user checks for performing operations on the API endpoints.

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,16 @@ var authenticate = require('../authenticate');
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+  User.find({}, function (err, users) {
+    var userMap = {};
+
+    users.forEach(function (user) {
+      userMap[user._id] = user;
+    });
+
+    res.send(userMap);
+  });
 });
 
 router.post('/signup', (req, res, next) => {
@@ -22,14 +30,14 @@ router.post('/signup', (req, res, next) => {
         res.json({ err: err });
       }
       else {
-        if(req.body.firstname){
+        if (req.body.firstname) {
           user.firstname = req.body.firstname
         }
-        if(req.body.lastname){
+        if (req.body.lastname) {
           user.lastname = req.body.lastname
         }
         user.save((err, user) => {
-          if(err){
+          if (err) {
             res.statusCode = 500;
             res.setHeader('Content-Type', 'application/json');
             res.json({ err: err });
@@ -47,15 +55,15 @@ router.post('/signup', (req, res, next) => {
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
 
-  var token = authenticate.getToken({_id: req.user._id})
+  var token = authenticate.getToken({ _id: req.user._id })
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, token: token, status: 'You are successfully logged in!'});
+  res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
 
-router.get('/logout', (req, res) => {
+router.get('/logout', (req, res, next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
