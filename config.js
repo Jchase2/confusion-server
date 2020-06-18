4b80d29f6450dfd42b06093b82a692262552fe6e
JChase2 <mr.jdchase@gmail.com>
2020-06-18 17:09:58 -0400

    Added passport oath login via fb

--- config.js
+++ config.js
@@ -2,3 +2,12 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion'
 }
+
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1677757175705741',
+        clientSecret: '05e26c1e02b53e5f24c7e9a5c5198e05'
+    }
+}
